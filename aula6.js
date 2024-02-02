aae84feb09f699476804ac70b27350ef19c21e0c
Henry <henrypollack1@gmail.com>
2024-02-01 23:14:11 -0300

    criando objetos

---
+++ aula6.js
@@ -0,0 +1,10 @@
+const henry  = {
+    nome: 'Henry Pollack',
+    idade: 25,
+    nivelDoCharNoTibia: 248
+};
+
+console.log(henry.nome);
+console.log(henry.idade);
+console.log(henry.nivelDoCharNoTibia);
+console.log(henry);
