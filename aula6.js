736aec8dbe82a661edca471122d66e141b3cf369
Henry <henrypollack1@gmail.com>
2024-02-02 22:37:26 -0300

    objeto dinamico

--- aula6.js
+++ aula6.js
@@ -1,10 +1,21 @@
-const henry  = {
+const pessoa  = {
     nome: 'Henry Pollack',
     idade: 25,
-    nivelDoCharNoTibia: 248
+    nivelDoCharNoTibia: 248,
+    descrever: function () {
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
+    }
 };
-
+pessoa.descrever = function(){
+    console.log(`Meu nome é ${this.nome}`)
+}
+/* henry.altura = 1.76;
+delete henry.nome;
 console.log(henry.nome);
 console.log(henry.idade);
 console.log(henry.nivelDoCharNoTibia);
-console.log(henry);
+console.log(henry);*/
+pessoa.nome = 'Heitor';
+pessoa.idade = 0.9;
+
+pessoa.descrever();
