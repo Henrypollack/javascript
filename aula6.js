40111fed0830b729eae2d7a9576f79326e105a17
Henry <henrypollack1@gmail.com>
2024-02-03 22:30:24 -0300

    classes e instancias

--- aula6.js
+++ aula6.js
@@ -1,4 +1,38 @@
-const pessoa  = {
+class Pessoa {
+    nome;
+    idade;
+    NoTibia;
+    constructor(nome,idade,charNoTibia){
+        this.nome = nome;
+        this.idade = idade;
+        this.NoTibia = charNoTibia;
+        this.anoNascimento = 2024 - idade;
+    }
+
+    descrever(){
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
+    }
+}
+const henry = new Pessoa('Henry Pollck',30,'Merdoxhero');
+const filho = new Pessoa('Heitor',0.9,'Milarra Luda');
+
+console.log(henry);
+console.log(filho);
+
+
+/*const henry = new Pessoa();
+henry.nome = 'Henry Pollack';
+henry.idade = 30;
+henry.charNoTibia = 'Merdoxhero';
+const heitor = new Pessoa();
+heitor.nome = 'Heitor de Oliveira';
+heitor.idade = 0.9;
+heitor.charNoTibia = 'Milarra Luda';
+console.log(henry);
+console.log(heitor);
+henry.descrever();
+heitor.descrever();
+const henry  = {
     nome: 'Henry Pollack',
     idade: 25,
     nivelDoCharNoTibia: 248,
@@ -9,13 +43,15 @@ const pessoa  = {
 pessoa.descrever = function(){
     console.log(`Meu nome é ${this.nome}`)
 }
-/* henry.altura = 1.76;
+console.log(pessoa['nome']);
+pessoa['nome'] = 'Heitor';
+console.log(pessoa['nome']);
+ henry.altura = 1.76;
 delete henry.nome;
 console.log(henry.nome);
 console.log(henry.idade);
 console.log(henry.nivelDoCharNoTibia);
-console.log(henry);*/
+console.log(henry);
 pessoa.nome = 'Heitor';
 pessoa.idade = 0.9;
-
-pessoa.descrever();
+pessoa.descrever();*/
