eb7708effda9cf3d21440f89d120d65c966532df
Henry <henrypollack1@gmail.com>
2024-02-07 21:20:49 -0300

    exemplos da aula 7 arrays incrementando e excluindo

---
+++ aula7.js
@@ -0,0 +1,22 @@
+
+const notas = [];
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+
+console.log(notas.length);
+
+
+/*const alunos = ['Jõao','Henry','Maria'];
+console.log(alunos);
+console.log(alunos.pop());
+console.log(alunos);
+console.log(alunos.shift());
+console.log(alunos);
+alunos.push('Heitor');
+alunos[4] = 'Andressa';
+console.log(alunos)
+alunos[2] = 'Beatriz';
+console.log(alunos);*/
